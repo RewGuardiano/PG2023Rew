9cf74cf4d71c62ca04cfa11fac92df06cddc09d5
STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>
2023-01-18 14:10:32 +0000

    Signed-off-by: STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>

--- PGRewFPS/Assets/FPSCameraScript.cs
+++ PGRewFPS/Assets/FPSCameraScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FPSCameraScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    float xyAngle, zAngle; 
     void Start()
     {
         
@@ -14,5 +16,17 @@ public class FPSCameraScript : MonoBehaviour
     void Update()
     {
         
+    }
+    internal void UpdatePosition(MainCharacterScript myMan, float horizontal, float vertical)
+    {
+        transform.position = myMan.transform.position;
+        //transform.rotation = myMan.transform.rotation;
+        xyAngle += horizontal;
+        zAngle += vertical;
+
+        zAngle = Mathf.Clamp(zAngle, -89f, 89);
+
+        transform.localRotation = Quaternion.Euler(zAngle, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, xyAngle, 0);
     }
 }
--- PGRewFPS/Assets/MainCharacterScript.cs
+++ PGRewFPS/Assets/MainCharacterScript.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class MainCharacterScript : MonoBehaviour
 {
-    float speed;
+    [SerializeField] Transform cameraTransform;
+    Vector2 look;
+    private float speed;
+    private float MouseSensitivity = 3f;
     const float Walking_speed = 3, Running_speed = 8, Crouch_speed = 3;
-
+  
     // Start is called before the first frame update
     void Start()
     {
         speed = Walking_speed;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -32,7 +36,24 @@ public class MainCharacterScript : MonoBehaviour
         {
             transform.position -= speed * transform.forward * Time.deltaTime;
         }
+        var input = new Vector3();
 
+        input = Vector3.ClampMagnitude(input, 1f);//so we won't move digonally faster than moving normally//
 
+        UpdateLook();
+       
+       
     }
+    void UpdateLook()
+    {
+        look.x += Input.GetAxisRaw("Mouse X") * MouseSensitivity;
+        look.y += Input.GetAxisRaw("Mouse Y") * MouseSensitivity;
+
+        look.y = Mathf.Clamp(look.y, -89f, 89);
+
+        cameraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, look.x, 0);
+       
+    }
+   
 }
