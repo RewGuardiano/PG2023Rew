4b521bfc8b3d34806e61d7be95d16d069444816b
STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>
2023-01-27 02:09:33 +0000

    Signed-off-by: STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>

---
+++ PGRewFPS/Assets/Scripts/MainCharacterScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterScript : MonoBehaviour
+{
+    FPSCameraScript Camera;
+    float speed;
+    const float Walking_speed = 3, Running_speed = 8, Crouch_speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Walking_speed;
+        Camera = FindObjectOfType<FPSCameraScript>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += speed * transform.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= speed * transform.right * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += speed * transform.right * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= speed * transform.forward * Time.deltaTime;
+        }
+
+        Camera.UpdatePosition(this, Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+    }
+
+  
+}
