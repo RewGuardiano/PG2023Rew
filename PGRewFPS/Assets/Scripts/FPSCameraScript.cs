0b06c7c8c4ffe1c70d517ad9e0521f6ca644a7d9
STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>
2023-02-06 21:28:06 +0000

    Signed-off-by: STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>

--- PGRewFPS/Assets/Scripts/FPSCameraScript.cs
+++ PGRewFPS/Assets/Scripts/FPSCameraScript.cs
@@ -20,28 +20,18 @@ public class FPSCameraScript : MonoBehaviour
     internal void UpdatePosition(MainCharacterScript myCharacter, float horizontal, float vertical)
     {
         transform.position = myCharacter.transform.position;
-        if (mouseNotActive(horizontal, vertical))
-        {
-            sensXY = Mathf.Lerp(sensXY, 0, 0.001f);
-            sensZ = Mathf.Lerp(sensZ, 0, 0.001f);
-        }
-        else
-        {
+       
 
             sensXY += horizontal;
             sensZ -= vertical;
             sensZ = Mathf.Clamp(sensZ, -45, 45);
 
-           
-        }
+          
+        
 
         transform.rotation = Quaternion.AngleAxis(sensXY, Vector3.up) * Quaternion.AngleAxis(sensZ, Vector3.right);
         // transform.LookAt(transform.position + transform.forward, Vector3.up);
     }
 
-    private bool mouseNotActive(float horizontal, float vertical)
-    {
-        return (Mathf.Abs(horizontal) + Mathf.Abs(vertical)) < 0.001f;
-    }
-  
+ 
 }
