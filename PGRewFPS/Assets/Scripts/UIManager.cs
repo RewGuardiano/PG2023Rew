868d62c0a57274ccbb12eb010c40bb381a0c9161
STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>
2023-05-04 07:18:50 +0100

    Signed-off-by: STUDENT Rewinell Guardiano <Rewinell.Guardiano@students.ittralee.ie>

--- PGRewFPS/Assets/Scripts/UIManager.cs
+++ PGRewFPS/Assets/Scripts/UIManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro; 
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -12,11 +13,33 @@ public class UIManager : MonoBehaviour
     TextMeshProUGUI killCounterTMP;
     [HideInInspector]
     public int KillCount;
-   
-  
+    public Transform PlayerPos;
+    public float spawnDistance = 2.0f; // The distance from the player to spawn the object
+    public float spawnHeight = 1.5f;
+    public Transform MinionCloneTemplate;
+    public GameObject YouWinMenu;
 
 
-   private void Awake()
+    public void EnableYouWinMenu()
+    {
+        YouWinMenu.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    private void OnEnable()
+    {
+        GameManagerScript.KillCountReached += EnableYouWinMenu;
+        
+        
+    }
+    private void OnDisable()
+    {
+        GameManagerScript.KillCountReached -= EnableYouWinMenu;
+       
+    }
+    private void Awake()
     {
         if(instance == null)
         {
@@ -36,7 +59,7 @@ public class UIManager : MonoBehaviour
     }
     void Start()
     {
-        
+        DontDestroyOnLoad(YouWinMenu);
     }
 
     // Update is called once per frame
@@ -44,6 +67,30 @@ public class UIManager : MonoBehaviour
     {
         
     }
-    
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void SupriseButton()
+    {
+        Vector3 spawnPosition = PlayerPos.position + PlayerPos.forward * spawnDistance;
+        spawnPosition.y = spawnHeight;
+
+
+        Quaternion spawnRotation = Quaternion.LookRotation(PlayerPos.position - spawnPosition);
+
+        Instantiate(MinionCloneTemplate, spawnPosition, spawnRotation);
+
+        YouWinMenu.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
 }
